fcad548cbfd782e5d0dd7d8d1630afe8e745f480
ayushupadhyay456 <ayushupadhyay456@gmail.com>
2026-07-01 00:43:03 +0530

    added login and signup

--- auth.py
+++ auth.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask,render_template,request
 from flask import Blueprint
+
 
 auth=Blueprint('auth',__name__)
 
 
-@auth.route('/')
+@auth.route('/login',methods=['get','post'])
 
 def login():
-    return 'this is the auth page'
+    data=request.form
+    print(data)
+    return render_template('login.html',user=None)
+
+@auth.route('/Sign-up')
+
+def signup():
+    return render_template('sign_up.html',user=None)
