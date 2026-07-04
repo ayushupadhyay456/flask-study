93d2678851dbf33b5113e41d1db39ffbc18880d7
ayushupadhyay456 <ayushupadhyay456@gmail.com>
2026-07-04 23:33:23 +0530

    fixed routing issue

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from os import path
 
 # 1. Initialize DB extension
@@ -8,26 +9,35 @@ DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
-    
+
     # 2. Configure app
     app.config['SECRET_KEY'] = 'dev-key-placeholder'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    
-    # 3. Bind the DB to this app
+
     db.init_app(app)
-    
+
     # 4. IMPORT BLUEPRINTS HERE (Breaks circular dependency)
     from auth import auth
-    from views import views  # assuming you have a views file too
-    
+    from views import views
+
     # 5. Register Blueprints
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(views, url_prefix='/')
-    
+
     # 6. Import models and create tables inside the app context
     from models import User, Note
     create_database(app)
-    
+
+    # 7. Set up Flask-Login (was missing entirely, even though models.py
+    #    uses UserMixin and auth.py needs login_user/current_user)
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
 
 def create_database(app):
--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from flask_login import UserMixin  #login using flask
 from sqlalchemy.sql import func
 
 class Note(db.Model):
-    id=db.Column(db.Integer,primaryKey=True)
+    id=db.Column(db.Integer,primary_key=True)
     data=db.Column(db.String(10000))
     date=db.Column(db.DateTime(timezone=True),default=func.now())
     user_id=db.Column(db.Integer,db.ForeignKey('user.id'))  #as the class will be referenced by user therefore lowercase u
@@ -12,10 +12,10 @@ class Note(db.Model):
 
 
 class User(db.Model,UserMixin):
-    id=db.Column(db.Integer,primaryKey=True)
+    id=db.Column(db.Integer,primary_key=True)
     email=db.Column(db.String(150),unique=True)
     password=db.Column(db.String(150))
-    first_name=db.Column(db.String(150))
+    firstName=db.Column(db.String(150))
     notes=db.relationship('Note')
 
 
