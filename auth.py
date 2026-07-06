92f47b44df7030513e61ea87805d3396ea20f105
ayushupadhyay456 <ayushupadhyay456@gmail.com>
2026-07-07 00:08:46 +0530

    comleted

--- auth.py
+++ auth.py
@@ -67,6 +67,6 @@ def signup():
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            return redirect(url_for('views.home'))
+            return redirect(url_for('views.home')) 
 
     return render_template('sign_up.html', user=current_user)
